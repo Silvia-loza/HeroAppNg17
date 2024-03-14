8b9e2ed6949eb5181bb9469ad252da6e7f93ba8d
Silvia Lozano <sylvilozano@gmail.com>
2024-03-15 00:59:55 +0100

    feat: Added setting to add new hero component to implement addHero service and some style fixes

--- src/app/heros/pages/hero-page/hero-page.component.ts
+++ src/app/heros/pages/hero-page/hero-page.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: ``,
+  styles: `.mat-card-img {
+    object-fit: cover;
+    width: 90%;
+    height: 70%;
+    margin-left: 5%;
+}`,
 })
 export class HeroPageComponent implements OnInit {
   public hero?: Hero;
--- src/app/heros/pages/new-page/new-page.component.ts
+++ src/app/heros/pages/new-page/new-page.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``,
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
   public publishers = [
     { id: 'Dc Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
+  constructor(private heroesService: HeroesService) {}
+  ngOnInit(): void {}
+
+  saveHero() {
+    console.log('form is valid', {
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value,
+    });
+    if (this.heroForm.valid) {
+      this.heroesService.addHero(<Hero>this.heroForm.value).subscribe(() => {
+        console.log('Hero saved', this.heroForm.value);
+        this.heroForm.reset();
+      });
+    }
+  }
 }
