578642e2da387661f7213f8d02248bcff474bf5c
Silvia Lozano <sylvilozano@gmail.com>
2024-03-15 00:37:56 +0100

    feat: add heroes CRUD

--- src/app/heros/services/heroes.service.ts
+++ src/app/heros/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -25,6 +25,25 @@ export class HeroesService {
   getSuggestions(query: string): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(
       `${this.baseUrl}/heroes?q=${query}&_limit=6`
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is reqired·');
+    return this.httpClient.patch<Hero>(
+      `${this.baseUrl}/heroes/${hero.id}`,
+      hero
+    );
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.httpClient.delete<boolean>(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError(() => of(false)),
+      map((resp) => true)
     );
   }
 }
