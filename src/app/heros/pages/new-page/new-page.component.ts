ab2208b4d746751b64fe729c121c503b118dfcf7
Silvia Lozano <sylvilozano@gmail.com>
2024-03-17 23:46:02 +0100

    feat: edit hero feature added

--- src/app/heros/pages/new-page/new-page.component.ts
+++ src/app/heros/pages/new-page/new-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
@@ -23,19 +25,47 @@ export class NewPageComponent implements OnInit {
     alt_img: new FormControl(''),
   });
 
-  constructor(private heroesService: HeroesService) {}
-  ngOnInit(): void {}
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heros/list']);
+
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
 
   saveHero() {
     console.log('form is valid', {
       formIsValid: this.heroForm.valid,
       value: this.heroForm.value,
     });
-    if (this.heroForm.valid) {
-      this.heroesService.addHero(<Hero>this.heroForm.value).subscribe(() => {
+    if (!this.heroForm.valid) return;
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(<Hero>this.heroForm.value).subscribe(() => {
+        //SNACKBAR
         console.log('Hero saved', this.heroForm.value);
         this.heroForm.reset();
       });
+      return;
     }
+    this.heroesService.addHero(<Hero>this.heroForm.value).subscribe(() => {
+      console.log('Hero saved', this.heroForm.value);
+      //snackBar + mavegar id
+      this.heroForm.reset();
+    });
   }
 }
